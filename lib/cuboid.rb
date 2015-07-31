4b25320c9f0329ebcf8c59660722e85e46f4854d
Jeremy Tan <jeremyeunsungdan@gmail.com>
2015-07-30 18:06:46 -0700

    Added origin, dimensions, length, width, height

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,27 @@
-
 class Cuboid
-  
-  #BEGIN public methods that should be your starting point
+  attr_reader :origin, :dimensions
+
+  def initialize(origin:, dimensions:)
+    @origin = origin
+    @dimensions = dimensions
+  end
+
+  def length
+    dimensions[0]
+  end
+
+  def width
+    dimensions[1]
+  end
+
+  def height
+    dimensions[2]
+  end
 
   def move_to!(x, y, z)
-  end
-  
-  def vertices
   end
   
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
   end
-
-  #END public methods that should be your starting point  
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,55 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
-#  Do whatever you need to do to make it work and please add your own test cases for as many
-#  methods as you feel need coverage
 describe Cuboid do
+  context 'when initialized without a specified origin' do
+    it 'raises an ArgumentError' do
+      expect { Cuboid.new }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#length' do
+    let(:origin) { [2, 1, 3] }
+    let(:dimensions) { [3, 4, 5] }
+    let(:cuboid) { Cuboid.new(origin: origin, dimensions: dimensions) }
+
+    it 'has the specified length' do
+      expect(cuboid.length).to eq(3)
+    end
+  end
+
+  describe '#width' do
+    let(:origin) { [2, 1, 3] }
+    let(:dimensions) { [3, 4, 5] }
+    let(:cuboid) { Cuboid.new(origin: origin, dimensions: dimensions) }
+
+    it 'has the specified width' do
+      expect(cuboid.width).to eq(4)
+    end
+  end
+
+  describe '#height' do
+    let(:origin) { [2, 1, 3] }
+    let(:dimensions) { [3, 4, 5] }
+    let(:cuboid) { Cuboid.new(origin: origin, dimensions: dimensions) }
+
+    it 'has the specified height' do
+      expect(cuboid.height).to eq(5)
+    end
+  end
+
+  describe '#vertices' do
+    let(:origin) { [2, 1, 3] }
+    let(:dimensions) { [3, 4, 5] }
+    let(:cuboid) { Cuboid.new(origin: origin, dimensions: dimensions) }
+
+    it 'has a list of vertices according to its origin and dimensions' do
+      expect(cuboid.vertices).to match_array([[2, 1, 3], [2, 1, 5], [2, 6, 3], [2, 6, 5], [5, 1, 3], [5, 1, 5], [5, 6, 3], [5, 6, 5]])
+    end
+  end
  
   describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
-    end
   end    
   
   describe "intersects?" do
   end
-
 end
